4ba6733d1bca9c99827b140c4993915fc885b588
Mrivalfirdaus <mrifalfirdaus17@gmail.com>
2023-02-06 11:50:20 +0700

    update 6feb 2023

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-let preveiwContainer = document.querySelector('.products-preview');
-let previewBox = preveiwContainer.querySelectorAll('.preview');
+let preveiwProfil = document.querySelector('.products-preview');
+let previewBox = preveiwProfil.querySelectorAll('.preview');
 
+document.querySelectorAll('.navbar .nav_right').forEach(nav_right =>{
+	nav_right.onclick = () =>{
+    preveiwProfil.style.display = 'flex';
+    let name = Profil.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
 
-// coba
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwProfil.style.display = 'none';
+  };
+});
 
 
 
@@ -12,3 +29,22 @@ var dd_main = document.querySelector(".dd_main");
 	dd_main.addEventListener("click", function(){
 		this.classList.toggle("active");
 	})
+
+
+
+	// navbar
+	const navbarNav = document.querySelector
+	("#navbar-nav");
+	document.querySelector('#hamburger-menu').
+	onclick = () => {
+		navbarNav.classList.toggle("active");
+	};
+
+	const hamburger = document.querySelector("#hamburger-menu");
+	
+	document.addEventListener('click', function(e){
+		if(!hamburger.contains(e.target)&& !navbarNav.contains(e.target)){
+		navbarNav.classList.remove('active');
+		}
+	});
+	
